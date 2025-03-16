b44b1e678e125d6a3b1fb28ef6e08faf800b042d
unknown <abisovfarid14@gmail.com>
2025-03-17 03:09:10 +0400

    user detail properties cart

--- src/pages/users/UserDetail.jsx
+++ src/pages/users/UserDetail.jsx
@@ -5,23 +5,29 @@ import { ToastContainer } from 'react-toastify'
 import { useParams } from "react-router-dom"
 import { useGetUserQuery } from "../../store/api/user.api"
 
-export default function UserDetail(){
-    const params=useParams()
-    const {data:user,...getUserFlags}=useGetUserQuery(params.id);
-    
+export default function UserDetail() {
+    const params = useParams()
+    const { data: user, ...getUserFlags } = useGetUserQuery(params.id);
+
     return <div>
-        {getUserFlags.isLoading ? <div>loading</div> : 
-         <div className="flex flex-col flex-1 bg-body-bg h-screen">
-                       <Header addBtnText='New Post' />
-            {user.name}
-            {user.email}
-                       <div className="p-5 "> 
-                       <div className='text-justify bg-white overflow-y-scroll  rounded-2xl h-container-h shadow-custom-shadow'>
-                         <PostManageTable userId={params.id}/> 
-                         <ToastContainer position="bottom-right" autoClose={1500} />
-                       </div>
+        {getUserFlags.isLoading ? <div>loading</div> :
+            <div className="flex flex-col flex-1 bg-body-bg h-screen">
+                <Header addBtnText='New Post' />
+                <div className='bg-white w-96 mt-3 ml-5 p-2 rounded-xl'>
+                    <ul>
+                        <li>Name : {user.name}</li>
+                        <li>Email : {user.email}</li>
+                        <li>Gender : {user.gender}</li>
+                        <li>Status : {user.status}</li>
+                    </ul>
+                </div>
+                <div className="p-5 ">
+                    <div className='text-justify bg-white overflow-y-scroll  rounded-2xl max-h-container-h shadow-custom-shadow'>
+                        <PostManageTable userId={params.id} />
+                        <ToastContainer position="bottom-right" autoClose={1500} />
                     </div>
-          </div>
+                </div>
+            </div>
         }
     </div>
 }
